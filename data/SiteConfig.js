8dfd34785a1805124bab246d20ffdd3e84cc8617
Deng Zhi Cheng <zhicheng.deng@siemens-healthineers.com>
2018-05-16 09:52:30 +0800

    prefix

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   siteLogo:
     "", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
   siteUrl: "https://haysclark.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
+  pathPrefix: "/danhua-blog/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription:
     "CFA，基金经理，数字货币投资人，《区块链数字货币投资指南》作者", // Website description used for RSS feeds/meta description tag.
   siteCover:
